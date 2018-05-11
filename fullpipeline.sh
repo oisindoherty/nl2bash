21d2022ba22ff78349350b8b12d9ffe1151d7dde
anabh4569 <anabh4569@gmail.com>
2018-05-10 22:10:41 -0700

    Created script to run full pipeline.

---
+++ fullpipeline.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+(./NL2BashWebScraper/gradlew run) &
+PID=$!
+read -p "Press enter to stop scraping."
+kill $!
+
+cd nl2bash_server
+pip3 install pipenv
+pipenv run python manage.py migrate
+pipenv run python -m nl2bash_server.add_data_from_scraper ../NL2BashWebScraper/ScrapedPages
+(pipenv run python manage.py runserver) &
+PID=$!
+read -p "Press enter to stop hosting the tester ui interface."
+kill $!
+pipenv run python -m nl2bash_server.save_data
+touch all.cm
+touch all.nl
+mv -f -t ../tellina/tellina_learning_module/data/bash all.cm all.nl
+cd ..
+
+cd tellina
+make submodule
+cd tellina_learning_module
+cd experiments
+# make data
+# make train
+make decode
+make gen_auto_evaluation_table
